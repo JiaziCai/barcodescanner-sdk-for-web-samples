005837a9f33b04bb191da05c22a44e95a73f570c
Jiazi Cai <caijiazi777@gmail.com>
2021-07-26 13:52:11 -0700

    update folder

--- vanilla-samples/settings-sample/src/config.js
+++
@@ -1,4 +0,0 @@
-export const Config = {
-    licenseKey: "YOUR_LICENSE_KEY_IS_NEEDED_HERE",
-    engineLocation: "https://cdn.jsdelivr.net/npm/scandit-sdk@5.x/build", // could also be e.g. "build"
-};
